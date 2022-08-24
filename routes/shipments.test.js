1bfd37bab5e5ee75c89ad817c4de1bc2ae4f7d07
Aishwarya Majumdar <aishu.maj1996@gmail.com>
2022-08-23 17:21:50 -0700

    mock tests

--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -1,11 +1,17 @@
 "use strict";
 
+const shipItApi = require("../shipItApi");
+
+shipItApi.shipProduct = jest.fn();
+
 const request = require("supertest");
 const app = require("../app");
 
 
 describe("POST /", function () {
   test("valid", async function () {
+    shipItApi.shipProduct.mockReturnValue(1000);
+    
     const resp = await request(app).post("/shipments").send({
       productId: 1000,
       name: "Test Tester",
@@ -13,10 +19,11 @@ describe("POST /", function () {
       zip: "12345-6789",
     });
 
-    expect(resp.body).toEqual({ shipped: expect.any(Number) });
+    expect(resp.body).toEqual({ shipped: 1000 });
   });
   
   test("invalid input", async function () {
+    
     const resp = await request(app).post("/shipments").send({
       productId: 34,
       name: "Test Tester",
